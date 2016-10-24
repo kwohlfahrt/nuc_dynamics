2d29142d84af6b8144d6ea3b95b43fd4393abbf9
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-10-24 16:56:49 +0100

    Specify numpy includes

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+from numpy import get_include as get_numpy_include
+
+exts = [Extension("nuc_cython", ["nuc_cython.pyx"],
+                  include_dirs=[get_numpy_include()])]
 
 setup(
-    ext_modules = cythonize("nuc_cython.pyx")
+    ext_modules = cythonize(exts)
 )
