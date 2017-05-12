e1a19b1b4d4ac57c180e9f416dbf3966a0fa089a
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-05-12 17:16:37 +0100

    Initial tests and cleanup of nuc_pdb
    Fix some obvious parenthesis issues, and write a test for nuc_pdb.

--- nuc_dynamics/nuc_pdb.py
+++ nuc_dynamics/nuc_pdb.py
@@ -13,7 +13,8 @@ def load_nuc_file(file_path, structure="0"):
     return coords, seq_pos, particle_size
 
 
-def export_pdb_coords(file_path, coords_dict, seq_pos_dict, particle_size, scale=1.0, extended=True):
+def export_pdb_coords(file_path, coords_dict, seq_pos_dict, particle_size,
+                      scale=1.0, extended=True):
 
     from numpy import array, uint32, float32
 
@@ -22,24 +23,21 @@ def export_pdb_coords(file_path, coords_dict, seq_pos_dict, particle_size, scale
     prefix = 'HETATM'
     line_format = '%-80.80s\n'
 
+    pdb_format = '%-6.6s%5.1d %4.4s%s%3.3s %s%4.1d%s   %8.3f%8.3f%8.3f%6.2f%6.2f          %2.2s  \n'
     if extended:
-        pdb_format = '%-6.6s%5.1d %4.4s%s%3.3s %s%4.1d%s   %8.3f%8.3f%8.3f%6.2f%6.2f          %2.2s  %10d\n'
-        ter_format = '%-6.6s%5.1d      %s %s%4.1d%s                                                     %10d\n'
-    else:
-        pdb_format = '%-6.6s%5.1d %4.4s%s%3.3s %s%4.1d%s   %8.3f%8.3f%8.3f%6.2f%6.2f          %2.2s  \n'
-        ter_format = '%-6.6s%5.1d      %s %s%4.1d%s                                                     \n'
+        pdb_format = pdb_format[:-1] + '%10d\n'
 
-    file_obj = open(file_path, 'w')
+    file_obj = file_path.open('w')
     write = file_obj.write
 
     chromosomes = list(sorted(seq_pos_dict.keys()))
     num_models = len(coords_dict[chromosomes[0]])
     title = 'NucDynamics genome structure export'
 
-    write(line_format % 'TITLE     %s' % title)
+    write(line_format % ('TITLE     %s' % title))
     write(line_format % 'REMARK 210')
     write(line_format % 'REMARK 210 Atom type C is used for all particles')
-    write(line_format % 'REMARK 210 Atom number increases every %s bases' % particle_size)
+    write(line_format % ('REMARK 210 Atom number increases every %s bases' % particle_size))
     write(line_format % 'REMARK 210 Residue code indicates chromosome')
     write(line_format % 'REMARK 210 Residue number represents which sequence Mb the atom is in')
     write(line_format % 'REMARK 210 Chain letter is different every chromosome, where Chr1=a, Chr2=b etc.')
@@ -52,8 +50,7 @@ def export_pdb_coords(file_path, coords_dict, seq_pos_dict, particle_size, scale
     pos_chromo = {}
 
     for m in range(num_models):
-        line = 'MODEL     %4d' % (m+1)
-        write(line_format  % line)
+        write(line_format % ('MODEL     %4d' % (m+1)))
 
         c = 0
         j = 1
@@ -141,7 +138,7 @@ def main(args=None):
     args = parser.parse_args(argv[1:] if args is None else args)
     coords, seq_pos, particle_size = load_nuc_file(args.input, args.structure)
     coords = {k: v * args.scale for k, v in coords.items()}
-    export_pdb_coords(str(args.output), coords, seq_pos, particle_size)
+    export_pdb_coords(args.output, coords, seq_pos, particle_size)
 
 if __name__ == "__main__":
     main()
