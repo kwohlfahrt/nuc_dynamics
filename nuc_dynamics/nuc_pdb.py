eae82bafd8b40a16b3f507ea264bde498cd5d43e
Kai Wohlfahrt <kjw53@cam.ac.uk>
2017-05-16 10:19:27 +0100

    Use context manager for pdb file

--- nuc_dynamics/nuc_pdb.py
+++ nuc_dynamics/nuc_pdb.py
@@ -31,77 +31,76 @@ def export_pdb_coords(file_path, coords_dict, seq_pos_dict, particle_size,
     if extended:
         pdb_format = pdb_format[:-1] + '%10d\n'
 
-    file_obj = file_path.open('w')
-    write = file_obj.write
+    with file_path.open('w') as file_obj:
+        write = file_obj.write
 
-    coords_dict = OrderedDict(sorted(coords_dict.items()))
-    seq_pos_dict = OrderedDict(sorted(seq_pos_dict.items()))
-    chromosomes = list(seq_pos_dict.keys())
-    num_models = len(next(iter(coords_dict.values())))
-    title = 'NucDynamics genome structure export'
+        coords_dict = OrderedDict(sorted(coords_dict.items()))
+        seq_pos_dict = OrderedDict(sorted(seq_pos_dict.items()))
+        chromosomes = list(seq_pos_dict.keys())
+        num_models = len(next(iter(coords_dict.values())))
+        title = 'NucDynamics genome structure export'
 
-    write(line_format % ('TITLE     %s' % title))
-    write(line_format % 'REMARK 210')
-    write(line_format % 'REMARK 210 Atom type C is used for all particles')
-    write(line_format % ('REMARK 210 Atom number increases every %s bases' % particle_size))
-    write(line_format % 'REMARK 210 Residue code indicates chromosome')
-    write(line_format % 'REMARK 210 Residue number represents which sequence Mb the atom is in')
-    write(line_format % 'REMARK 210 Chain letter is different every chromosome, where Chr1=A, Chr2=B etc.')
+        write(line_format % ('TITLE     %s' % title))
+        write(line_format % 'REMARK 210')
+        write(line_format % 'REMARK 210 Atom type C is used for all particles')
+        write(line_format % ('REMARK 210 Atom number increases every %s bases' % particle_size))
+        write(line_format % 'REMARK 210 Residue code indicates chromosome')
+        write(line_format % 'REMARK 210 Residue number represents which sequence Mb the atom is in')
+        write(line_format % 'REMARK 210 Chain letter is different every chromosome, where Chr1=A, Chr2=B etc.')
 
-    if extended:
-        file_obj.write(line_format % 'REMARK 210 Extended PDB format with particle seq. pos. in last column')
+        if extended:
+            file_obj.write(line_format % 'REMARK 210 Extended PDB format with particle seq. pos. in last column')
 
-    file_obj.write(line_format % 'REMARK 210')
+        file_obj.write(line_format % 'REMARK 210')
 
-    for m in range(num_models):
-        write(line_format % ('MODEL     %4d' % (m+1)))
+        for m in range(num_models):
+            write(line_format % ('MODEL     %4d' % (m+1)))
 
-        c = 0
-        seqPrev = None
-        for chromo in chromosomes:
-            if chromo.isdigit():
-                chain_code = ascii_uppercase[int(chromo) - 1]
-            elif len(chromo) == 1:
-                chain_code = chromo.upper()
-            else:
-                chain_code = ascii_lowercase[chromosomes.index(chromo)]
+            c = 0
+            seqPrev = None
+            for chromo in chromosomes:
+                if chromo.isdigit():
+                    chain_code = ascii_uppercase[int(chromo) - 1]
+                elif len(chromo) == 1:
+                    chain_code = chromo.upper()
+                else:
+                    chain_code = ascii_lowercase[chromosomes.index(chromo)]
 
-            resName = chromo[:3]
-            if len(chromo) < 2:
-                resName = resName.rjust(2, '_')
-            if len(chromo) < 3:
-                resName = 'C' + resName
+                resName = chromo[:3]
+                if len(chromo) < 2:
+                    resName = resName.rjust(2, '_')
+                if len(chromo) < 3:
+                    resName = 'C' + resName
 
-            pos = seq_pos_dict[chromo]
-            coords = coords_dict[chromo][m]
-            if len(pos) != len(coords):
-                raise ValueError("Sequence position and coordinates have different length.")
+                pos = seq_pos_dict[chromo]
+                coords = coords_dict[chromo][m]
+                if len(pos) != len(coords):
+                    raise ValueError("Sequence position and coordinates have different length.")
 
-            for i, ((x, y, z), seqPos) in enumerate(zip(coords, pos)):
-                c += 1
+                for i, ((x, y, z), seqPos) in enumerate(zip(coords, pos)):
+                    c += 1
 
-                seqMb = int(seqPos//1e6) + 1
-                j = j+1 if seqMb == seqPrev else 1
-                seqPrev = seqMb
+                    seqMb = int(seqPos//1e6) + 1
+                    j = j+1 if seqMb == seqPrev else 1
+                    seqPrev = seqMb
 
-                aName = '%-3s' % ('C%d' % j)
+                    aName = '%-3s' % ('C%d' % j)
 
-                data = [prefix,c,aName,alc,resName,chain_code,seqMb,ins,x,y,z,0.0,0.0,el]
-                if extended:
-                    data.append(seqPos)
-                write(pdb_format % tuple(data))
+                    data = [prefix,c,aName,alc,resName,chain_code,seqMb,ins,x,y,z,0.0,0.0,el]
+                    if extended:
+                        data.append(seqPos)
+                    write(pdb_format % tuple(data))
 
-        write(line_format  % 'ENDMDL')
+            write(line_format  % 'ENDMDL')
 
-    chromo_offsets = chain([0], accumulate(map(len, seq_pos_dict.values())))
-    chromo_lengths = map(len, seq_pos_dict.values())
-    for offset, chromo_len in zip(chromo_offsets, chromo_lengths):
-        for i in range(1, chromo_len):
-            line = 'CONECT%5.1d%5.1d' % (offset+i, offset+i+1)
-            write(line_format % line)
+        chromo_offsets = chain([0], accumulate(map(len, seq_pos_dict.values())))
+        chromo_lengths = map(len, seq_pos_dict.values())
+        for offset, chromo_len in zip(chromo_offsets, chromo_lengths):
+            for i in range(1, chromo_len):
+                line = 'CONECT%5.1d%5.1d' % (offset+i, offset+i+1)
+                write(line_format % line)
 
-    write(line_format  % 'END')
-    file_obj.close()
+        write(line_format  % 'END')
 
 
 def main(args=None):
